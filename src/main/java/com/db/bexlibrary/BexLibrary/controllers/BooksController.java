49615bec0a776c268c77dc629600ecdf0c356a64
Ana <ana.georgia.turcus@gmail.com>
2018-05-12 22:03:49 +0300

    controllers-login&book

---
+++ src/main/java/com/db/bexlibrary/BexLibrary/controllers/BooksController.java
@@ -0,0 +1,52 @@
+package com.db.bexlibrary.BexLibrary.controllers;
+
+import com.db.bexlibrary.BexLibrary.entities.Book;
+import com.db.bexlibrary.BexLibrary.entities.GlobalVariables;
+import com.db.bexlibrary.BexLibrary.repositories.BookRepository;
+import com.db.bexlibrary.BexLibrary.service.BookService;
+import jdk.nashorn.internal.objects.Global;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
+public class BooksController {
+
+
+    @Autowired
+    private BookRepository bookRepo;
+
+    @Autowired
+    private BookService bookService;
+
+
+    @GetMapping(value = "/books")
+    public List<Book> findBooks() {
+        return bookRepo.findAll();
+    }
+
+    @GetMapping(value = "/isLoggedIn")
+    public ResponseEntity<Boolean> isLoggedIn() {
+        return new ResponseEntity<Boolean>(GlobalVariables.getInstance().getEmail() != null && !GlobalVariables.getInstance().getEmail().isEmpty() , HttpStatus.OK );
+    }
+
+    @GetMapping(value = "/logOut")
+    public ResponseEntity<Boolean> logOut() {
+        GlobalVariables.getInstance().setEmail(null);
+        return new ResponseEntity<Boolean>(true, HttpStatus.OK );
+    }
+
+
+
+    @GetMapping(value = "/booksbyauthor")
+    public List<Book> findBookByAuthor(@RequestParam(name="name")String name) {
+        return bookService.findBooksByAuthorName(name);
+    }
+
+
+
+}
---
+++ src/main/java/com/db/bexlibrary/BexLibrary/controllers/LoginController.java
@@ -0,0 +1,33 @@
+package com.db.bexlibrary.BexLibrary.controllers;
+
+import com.db.bexlibrary.BexLibrary.entities.GlobalVariables;
+import com.db.bexlibrary.BexLibrary.entities.User;
+import com.db.bexlibrary.BexLibrary.entities.UserPOJO;
+import com.db.bexlibrary.BexLibrary.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
+public class LoginController{
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<String> getLogin(@RequestBody UserPOJO userPOJO) {
+
+        User user = userRepository.findUserByEmail(userPOJO.getUserEmail());
+
+        if (userPOJO.getPassword().equals(user.getPassword())) {
+            GlobalVariables.getInstance().setEmail(userPOJO.getUserEmail());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+}
